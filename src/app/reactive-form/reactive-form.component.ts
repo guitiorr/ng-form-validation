67c021ba3b8fd531112cbce78cdd1a9567db406e
guitiorr <officialglude12345@gmail.com>
2025-02-19 10:03:21 +0700

    base reactive form done

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.scss'
 })
 export class ReactiveFormComponent {
 
+  userForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    username: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl('Jogja'),
+    zip: new FormControl(''),
+    isTermsAgreed: new FormControl(false),
+  })
+
+  onUserSave(){
+    console.log(this.userForm);
+  }
 }
