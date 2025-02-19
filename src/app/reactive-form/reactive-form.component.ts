af9e75ddff5b0c750851136f5d13a75ff2e12ed7
guitiorr <officialglude12345@gmail.com>
2025-02-19 10:29:06 +0700

    validation for reactive-form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -10,13 +10,13 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class ReactiveFormComponent {
 
   userForm: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    username: new FormControl(''),
-    city: new FormControl(''),
-    state: new FormControl('Jogja'),
-    zip: new FormControl(''),
-    isTermsAgreed: new FormControl(false),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    city: new FormControl('', [Validators.required]),
+    state: new FormControl('Jogja', [Validators.required]),
+    zip: new FormControl('', [Validators.required]),
+    isTermsAgreed: new FormControl(false, [Validators.required]),
   })
 
   onUserSave(){
