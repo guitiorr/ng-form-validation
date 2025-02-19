5a979bf9e7aac192f112a7c0c27e579692a71281
guitiorr <officialglude12345@gmail.com>
2025-02-19 09:46:55 +0700

    modify routing and add nav for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TemplateFormComponent } from './template-form/template-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TemplateFormComponent],
+  imports: [RouterOutlet, TemplateFormComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'template', pathMatch: 'full'},
   {path: 'template', component: TemplateFormComponent},
+  {path: 'reactive', component: ReactiveFormComponent},
 ];
